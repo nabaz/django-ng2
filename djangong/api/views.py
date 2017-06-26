9a8c3d890cc0a07c4a1155c9c3124703058b46fa
Nabaz Maaruf <nabaz@idevelopwebsite.com>
2017-06-26 14:48:46 -0400

    fixed git issue

--- djangong/api/views.py
+++ djangong/api/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from api.models import Post
+from api.serializes import PostSerializer
+import pdb
 
-# Create your views here.
+@api_view(['GET', 'POST'])
+def post_list(request):
+    if request.method = 'GET':
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many=True)
+
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        pdb.set_trace()
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
